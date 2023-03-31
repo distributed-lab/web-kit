ad5a7c152bb42b4735e7681e6397cb6cfb6bca96
Semen Loktionov <loktionov.kh@gmail.com>
2023-03-31 15:17:11 +0300

    remove test code, ts-node deps, fix npmignore

--- packages/fetcher/src/index.ts
+++ packages/fetcher/src/index.ts
@@ -1,43 +1,5 @@
-import { Fetcher } from '@/fetcher'
-
-export * from './abort-manager'
 export * from './const'
 export * from './enums'
+export * from './error'
 export * from './fetcher'
-export * from './helpers'
-export * from './response-builder'
 export * from './types'
-
-const fetcher = new Fetcher({
-  baseUrl: 'https://jsonplaceholder.typicode.com',
-})
-
-fetcher.useInterceptor({
-  request: async request => {
-    return { ...request, url: `${request.url}?foo=bar` }
-  },
-  response: async response => {
-    if (response.ok) {
-      return response
-    }
-
-    return fetcher.get('/auth/refresh')
-  },
-  error: async response => {
-    if (response.status === 401) {
-      return fetcher.get('/auth/refresh')
-    }
-
-    return response
-  },
-})
-
-fetcher.get(
-  '/posts/1',
-  {},
-  {
-    headers: {
-      'X-Test': 'test',
-    },
-  },
-)
