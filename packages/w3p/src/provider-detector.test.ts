9ed78ba7b9231c212183a035844cca30b60973e6
nikitabelous <nikitabelous16@gmail.com>
2023-04-26 12:43:35 +0300

    WIP

---
+++ packages/w3p/src/provider-detector.test.ts
@@ -0,0 +1,29 @@
+import { ProviderDetector } from '@/provider-detector'
+import { CoinbaseProvider, MetamaskProvider } from '@/providers'
+
+describe('performs provider detector unit test', () => {
+  describe('performs init', () => {
+    let providerDetector: ProviderDetector
+
+    beforeEach(() => {
+      providerDetector = new ProviderDetector()
+    })
+
+    test('should initialize the provider detector', async () => {
+      const spyDetectRawProviders = jest.spyOn(
+        providerDetector,
+        '#detectRawProviders',
+      )
+      const spyDefineProviders = jest.spyOn(
+        providerDetector,
+        '#defineProviders',
+      )
+
+      await providerDetector.init()
+
+      expect(spyDetectRawProviders).toHaveBeenCalledTimes(1)
+      expect(spyDefineProviders).toHaveBeenCalledTimes(1)
+      expect(providerDetector.isInitiated).toBe(true)
+    })
+  })
+})
